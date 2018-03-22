bdc04d2902ad86c2b67b2c13487c9db3192b1d81
AlexeyKarasev <karasev.alexey@gmail.com>
2018-03-22 17:07:55 +0300

    fixed some bugs

--- src/services/s3/mod.rs
+++ src/services/s3/mod.rs
@@ -11,7 +11,6 @@ use futures::future::Future;
 use tokio_core::reactor::Handle;
 use rusoto_core::request::{HttpClient, TlsError};
 use rusoto_core::region::Region;
-use rusoto_core::RusotoFuture;
 use rusoto_s3::{PutObjectError, PutObjectOutput, PutObjectRequest, S3 as S3Trait, S3Client};
 use image;
 use image::GenericImage;
@@ -27,10 +26,10 @@ enum Size {
 impl Display for Size {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         match self {
-            Thumb => f.write_str("thumb"),
-            Small => f.write_str("small"),
-            Medium => f.write_str("medium"),
-            Large => f.write_str("large"),
+            &Size::Thumb => f.write_str("thumb"),
+            &Size::Small => f.write_str("small"),
+            &Size::Medium => f.write_str("medium"),
+            &Size::Large => f.write_str("large"),
         }
     }
 }
@@ -52,37 +51,41 @@ impl S3 {
         })
     }
 
-    fn create_aws_name(prefix: &str, image_type: &str, size: Option<&Size>) -> String {
+    fn generate_random_hash() -> String {
         let mut name_bytes = vec![0; HASH_LEN_BYTES as usize];
         let buffer = name_bytes.as_mut_slice();
         rand::thread_rng().fill_bytes(buffer);
+        Self::encode_for_aws(&encode(buffer))
+    }
+
+    fn create_aws_name(prefix: &str, image_type: &str, size: Option<&Size>, random_hash: &str) -> String {
         let name = match size {
-            Some(size) => format!("{}-{}-{}.{}", prefix, encode(buffer), size, image_type),
-            None => format!("{}-{}.{}", prefix, encode(buffer), image_type),
+            Some(size) => format!("{}-{}-{}.{}", prefix, random_hash, size, image_type),
+            None => format!("{}-{}.{}", prefix, random_hash, image_type),
         };
         name
     }
 
-    fn upload_image_with_size(&self, size: Option<&Size>, bucket: &str, content_type: &str, image_type: &str, bytes: Vec<u8>) -> Box<Future<Item = PutObjectOutput, Error = PutObjectError>> {
-        let name = Self::create_aws_name("img", image_type, size);
+    fn upload_image_with_size(&self, size: Option<&Size>, bucket: &str, content_type: &str, image_type: &str, random_hash: &str, bytes: Vec<u8>) -> Box<Future<Item = PutObjectOutput, Error = PutObjectError>> {
+        let name = Self::create_aws_name("img", image_type, size, random_hash);
         self.raw_upload(bucket.to_string(), name, Some(content_type.to_string()), bytes)
     }
 
     pub fn upload_image(&self, image_type: &str, bytes: Vec<u8>) -> Box<Future<Item = String, Error = PutObjectError>> {
         let content_type = format!("image/{}", image_type);
         let bucket = "storiqa-dev";
-        let name = Self::create_aws_name("img", image_type, None);
-        let url_encoded_name = Self::url_encode_base64(&name);
+        let random_hash = Self::generate_random_hash();
+        let name = Self::create_aws_name("img", image_type, None, &random_hash);
         let url = format!(
             "https://s3.amazonaws.com/{}/{}",
-            bucket, url_encoded_name
+            bucket, name
         );
         if let Ok(image_hash) = Self::prepare_image(image_type, &bytes[..]) {
             let mut futures: Vec<_> = image_hash.keys().map(|size| {
                 let bytes = image_hash.get(size).unwrap().to_vec();
-                self.upload_image_with_size(Some(size), bucket, &content_type, image_type, bytes)
+                self.upload_image_with_size(Some(size), bucket, &content_type, image_type, &random_hash, bytes)
             }).collect();
-            futures.push(self.upload_image_with_size(None, bucket, &content_type, image_type, bytes));
+            futures.push(self.upload_image_with_size(None, bucket, &content_type, image_type, &random_hash, bytes));
             Box::new(future::join_all(futures).map(move |_| url))
         } else {
             Box::new(future::err(PutObjectError::Unknown("failed to set image sizes".to_string()))) as Box<Future<Item = String, Error = PutObjectError>>
@@ -146,9 +149,9 @@ impl S3 {
         Ok(hash)
     }
 
-    fn url_encode_base64(s: &str) -> String {
-        let s = s.replace("+", "%2B");
-        let s = s.replace("/", "%2F");
-        s.replace("=", "%3D")
+    fn encode_for_aws(s: &str) -> String {
+        let s = s.replace("+", "A");
+        let s = s.replace("/", "B");
+        s.replace("=", "C")
     }
 }
