09e2e675067806834e034ae653b5aa976c7221cc
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2019-01-23 16:58:46 +0300

    Move integration tests to one package to reduce compilation time (#52)

---
+++ tests/testcases/main.rs
@@ -0,0 +1,18 @@
+extern crate chrono;
+extern crate futures;
+extern crate futures_timer;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate jsonwebtoken;
+extern crate mime;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate statics_lib as lib;
+extern crate stq_http;
+extern crate tokio_core;
+
+mod common;
+
+mod images_test;
